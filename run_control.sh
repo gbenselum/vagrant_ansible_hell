13a756b3426135296203e12d639f40aade2b29e8
Gabrielzeven <49731558+gbenselum@users.noreply.github.com>
2020-08-31 15:42:07 -0300

    Update run_control.sh

--- run_control.sh
+++ run_control.sh
@@ -3,7 +3,9 @@
 # to start playing run: "vagrant ssh control"
 
 # send insecure KEY to control node
+
 scp ~/.vagrant.d/insecure_private_key vagrant@192.168.56.106:~/.ssh/id_rsa
+ssh vagrant@192.168.56.106 'chmod 700 ~/.ssh/id_rsa'
 
 # send inventory to control node
 curl https://raw.githubusercontent.com/gbenselum/diversity_ansible_lab/master/hosts --output ./inventory
