73b1d3f41a555c3151d8e3ea08a9625197adb44a
Gabrielzeven <49731558+gbenselum@users.noreply.github.com>
2020-08-31 20:40:03 -0300

    Update run_control.sh

--- run_control.sh
+++ run_control.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # This script must run your host 
-# to start playing run: "vagrant ssh control"
+# After Run, start with: "vagrant ssh control"
 
-# send insecure KEY to control node
 
+# send vagrant insecure KEY to control node
 scp ~/.vagrant.d/insecure_private_key vagrant@192.168.56.106:~/.ssh/id_rsa
 ssh vagrant@192.168.56.106 'chmod 700 ~/.ssh/id_rsa'
 
 # send inventory to control node
 curl https://raw.githubusercontent.com/gbenselum/diversity_ansible_lab/master/hosts --output ./inventory
 scp ./inventory vagrant@192.168.56.106:~/inventory
+rm inventory
 
-
-# installs pip3 and then ansible
+# installs pip3 and then ansible on control node
 ssh vagrant@192.168.56.106 'sudo yum install python3 -y; sudo pip3 install ansible ansible-lint'
 
+# remmeber to press "yes" running your first ansible command
