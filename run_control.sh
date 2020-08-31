65bcd3fa24225473393ff5b3146911525cde5e60
Gabrielzeven <49731558+gbenselum@users.noreply.github.com>
2020-08-31 15:12:03 -0300

    Update run_control.sh

--- run_control.sh
+++ run_control.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script must run in control node 
 # from vagrant host, vagrant ssh control
-scp ~/.vagrant.d/insecure_private_key vagrant@192.168.56.106:~/.ssh/id_rsa
+# scp ~/.vagrant.d/insecure_private_key vagrant@192.168.56.106:~/.ssh/id_rsa
 curl https://raw.githubusercontent.com/gbenselum/diversity_ansible_lab/master/hosts --output ~/inventory
 sudo yum install python3 -y
 sudo pip3 install ansible ansible-lint
