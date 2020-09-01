176877155428c1e623152e643abad7738de8c20b
Gabrielzeven <49731558+gbenselum@users.noreply.github.com>
2020-09-01 15:04:10 -0300

    Update run_host.sh

--- run_host.sh
+++ run_host.sh
@@ -2,7 +2,8 @@
 # This script must run your host 
 # After Run, start with: "vagrant ssh control"
 
-ssh-keygen -f "~/.ssh/known_hosts" -R "192.168.56.106"
+#clean up ssh keys if is not the first run
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "192.168.56.106"
 
 # send vagrant insecure KEY to control node
 scp ~/.vagrant.d/insecure_private_key vagrant@192.168.56.106:~/.ssh/id_rsa
