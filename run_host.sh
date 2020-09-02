436f77afec89a0c5e746899edf20343f27cf33fe
Gabriel <gabriel@gbenselu.localdomain>
2020-09-02 02:28:11 -0300

    new

--- run_host.sh
+++ run_host.sh
@@ -10,13 +10,18 @@ ssh-keygen -f "$HOME/.ssh/known_hosts" -R "192.168.56.106"
 scp ~/.vagrant.d/insecure_private_key vagrant@192.168.56.106:~/.ssh/id_rsa
 ssh vagrant@192.168.56.106 'chmod 700 ~/.ssh/id_rsa'
 
-# send inventory to control node
-curl https://raw.githubusercontent.com/gbenselum/diversity_ansible_lab/master/hosts --output ./inventory
-scp ./inventory vagrant@192.168.56.106:~/inventory
-rm inventory
+
+
+# send inventory to control node #### better way, git cloning full folder
+# curl https://raw.githubusercontent.com/gbenselum/diversity_ansible_lab/master/hosts --output ./inventory
+# scp ./inventory vagrant@192.168.56.106:~/inventory
+# rm inventory
 
 # installs git, pip3 and then ansible on control node
 ssh vagrant@192.168.56.106 'sudo yum install python3 git -y; sudo pip3 install ansible ansible-lint'
+
+# git clone on the control node
+ssh vagrant@192.168.56.106 'git clone https://github.com/gbenselum/vagrant_ansible_hell.git'
 
 # remmeber to press "yes" running your first ansible command
 
